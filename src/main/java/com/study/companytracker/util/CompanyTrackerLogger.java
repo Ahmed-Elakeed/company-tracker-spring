0926e46d6f9d724ca7135d0bde204ff2d4433d44
ahmed_zakaria <ahmed_zakaria@rayacorp.com>
2023-10-02 13:23:02 +0300

    Application main logger Java Docs

--- src/main/java/com/study/companytracker/util/CompanyTrackerLogger.java
+++ src/main/java/com/study/companytracker/util/CompanyTrackerLogger.java
@@ -3,10 +3,21 @@ package com.study.companytracker.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
+/**
+ * @Author Ahmed Elakeed
+ * Singleton Application Logger
+ */
 public class CompanyTrackerLogger {
 
+    // Initialize final logger instance
     private static final Logger CompanyTrackerLogger = LoggerFactory.getLogger(CompanyTrackerLogger.class);
 
+
+    /**
+     * @Author Ahmed Elakeed
+     * @return single logger instance to use
+     */
     public static Logger LOGGER(){
         return CompanyTrackerLogger;
     }
