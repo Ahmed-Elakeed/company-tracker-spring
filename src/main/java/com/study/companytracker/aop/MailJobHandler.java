c2eb05682f74d826666718ce00e17448add03695
ahmed_zakaria <ahmed_zakaria@rayacorp.com>
2023-10-03 14:38:31 +0300

    exception mail job

---
+++ src/main/java/com/study/companytracker/aop/MailJobHandler.java
@@ -0,0 +1,40 @@
+package com.study.companytracker.aop;
+
+import com.study.companytracker.dto.GenericRestResponse;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import java.util.List;
+
+@Aspect
+@Configuration
+@RequiredArgsConstructor
+public class MailJobHandler {
+
+    @Value("#{'${receivers.mails}'.split(',')}")
+    private List<String> mails;
+
+    private final JavaMailSender mailSender;
+
+
+    @AfterReturning(value = "com.study.companytracker.configuration.PointCutConfiguration.exceptionHandlerPointCut()",returning = "genericRestResponse")
+    public void exceptionMailHandler(GenericRestResponse<?> genericRestResponse){
+        System.out.println(this.mails);
+        this.mails.forEach(mail -> this.sendMail(mail,genericRestResponse));
+    }
+
+    private void sendMail(String to, GenericRestResponse<?> genericRestResponse) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("dev@company.com");
+        message.setTo(to);
+        message.setSubject(genericRestResponse.getResponseMessage().toString());
+        message.setText(genericRestResponse.getErrorMessage());
+        mailSender.send(message);
+    }
+
+}
---
+++ src/main/java/com/study/companytracker/configuration/MailConfiguration.java
@@ -0,0 +1,36 @@
+package com.study.companytracker.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+
+    @Value("${app.mail}")
+    private String appMail;
+    @Value("${app.password}")
+    private String appPassword;
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(appMail);
+        mailSender.setPassword(appPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+}
