436920d4b823ffd4a8ca125e12741db834167680
Mo'men magdy <momenmagdy464@gmail.com>
2023-10-08 14:34:41 +0300

    add department converter and update employee converter

--- src/main/java/com/study/companytracker/model/Department.java
+++ src/main/java/com/study/companytracker/model/Department.java
@@ -2,10 +2,7 @@ package com.study.companytracker.model;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
@@ -15,6 +12,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @ToString
 public class Department {
 
